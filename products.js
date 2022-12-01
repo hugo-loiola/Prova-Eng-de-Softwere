657c6f0b9437967b7da3f9b32c7a3e6ae98133d5
Hugo Loiola de Queiroz <hugoloiola.1@gmail.com>
2022-12-01 20:29:33 -0300

    [finishes #183939837] Feito Listar por categoria

--- products.js
+++ products.js
@@ -1,34 +1,43 @@
-let scanf = require('scanf');
+let scanf = require("scanf");
 
 const produtos = [
-    {
-        nome: "Notebook",
-        categoria: "Eletrônico",
-        valor: 1999.99
-    },
-    {
-        nome: "Impressora",
-        categoria: "Eletrônico",
-        valor: 999.99
-    },
-    {
-        nome: "Caneta BIC",
-        categoria: "Escolar",
-        valor: 0.50
-    },
-    {
-        nome: "Lapiseira Pentel",
-        categoria: "Escolar",
-        valor: 7.50
-    },
-]
-
+  {
+    nome: "Notebook",
+    categoria: "Eletrônico",
+    valor: 1999.99,
+  },
+  {
+    nome: "Impressora",
+    categoria: "Eletrônico",
+    valor: 999.99,
+  },
+  {
+    nome: "Caneta BIC",
+    categoria: "Escolar",
+    valor: 0.5,
+  },
+  {
+    nome: "Lapiseira Pentel",
+    categoria: "Escolar",
+    valor: 7.5,
+  },
+];
 
 function listar() {
-    produtos.forEach(p => {
-        console.log(p)
-    })
+  produtos.forEach((p) => {
+    console.log(p);
+  });
 }
+listar();
 
+const listar_por_categoria = (categoria) => {
+  for (let i = 0; i < produtos.length; i++) {
+    if (categoria == produtos[i].categoria) {
+      console.log(produtos[i].categoria);
+    } else if (categoria !== produtos[i].categoria) {
+      console.log("Não encontrado");
+    }
+  }
+};
 
-
+listar_por_categoria("Eletrônico");
